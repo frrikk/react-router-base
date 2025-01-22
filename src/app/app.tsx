45cbe2bf1a9b81761e5a3c3ffee54f98b4148713
frrikk <frikk.a.odlolien@tietoevry.com>
2025-01-22 21:49:29 +0100

    feat: adds navigation example

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,21 @@
 import * as React from "react";
+import { NavLink, Outlet } from "react-router";
 import AppRoutes from "@/pages/routes";
+import { cn } from "@/utils/cn";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <header>
+        <nav className={cn("flex gap-4")}>
+          <NavLink to="dashboard">Dashboard</NavLink>
+          <NavLink to="auth">Auth</NavLink>
+        </nav>
+      </header>
+      <main>
+        <AppRoutes />
+        <Outlet />
+      </main>
+    </>
+  );
 }
